5de768c9f52f26a6e90d920c37815445aff694c7
mayanksaxena90 <mayanksaxena90@gmail.com>
2014-07-19 11:58:28 +0530

    Added File Uploading functionality

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ var app = {},
     express = require("express"),
     redis = require('redis'),
     redisServer = require('./lib/redisserver'),
-    radio = require('./lib/radio');
+    radio = require('./lib/radio'),
+    utility = require('./lib/util'),
+    formidable = require('formidable'),
+    fs = require("fs");
 
 app.server = express();
 
@@ -15,6 +18,28 @@ app.server.configure(function(){
 
 app.server.get("/", function(req, res){
     res.send("Hello World");
+});
+
+app.server.post("/upload", function(req, res) {
+    console.log("In upload request handler");
+    var form = new formidable.IncomingForm();
+    form.parse(req, function(error, fields, files) {
+        var uniqueFileName = utility.guid();
+        var ext = files["file-0"].name.split(/[. ]+/).pop();
+        uniqueFileName = uniqueFileName + "." + ext;
+        console.log("parsing done:" + "public/audio/"+uniqueFileName);
+        var is = fs.createReadStream(files["file-0"].path);
+        var os = fs.createWriteStream("public/audio/"+uniqueFileName);
+
+        is.pipe(os);
+        is.on('end',function() {
+            fs.unlinkSync(files["file-0"].path);
+        });
+
+        fs.link("public/images/"+uniqueFileName, './test.png');
+        res.send({'audio': uniqueFileName});
+        res.end();
+    });
 });
 
 app.redisServer = new redisServer();
